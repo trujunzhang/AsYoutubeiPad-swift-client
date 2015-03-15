9cc4a73054dbd3b86c751967f10dfeeafe4a6d49
wanghaogithub720 <wanghaomspace@gmail.com>
2015-03-15 22:54:36 +0800

    swift.

---
+++ modules/Business-Logic-Layer/Pod/Classes/tabbars/SubscriptionsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SubscriptionsViewController.swift
+//  AsYoutubeiPadSwiftClient
+//
+//  Created by djzhang on 3/15/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import UIKit
+
+class SubscriptionsViewController: UIViewController {
+
+    @IBOutlet var menuButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+
+        // Do any additional setup after loading the view.
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
