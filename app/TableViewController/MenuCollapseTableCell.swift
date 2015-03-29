b35583db02467825268f03203a010f597d189116
wanghaogithub720 <wanghaomspace@gmail.com>
2015-03-29 17:11:18 +0800

    xxx

--- app/TableViewController/MenuCollapseTableCell.swift
+++ app/TableViewController/MenuCollapseTableCell.swift
@@ -11,20 +11,20 @@ import UIKit
 import Haneke
 
 class MenuCollapseTableCell: UITableViewCell {
-
+    
     @IBOutlet var iconImage: UIImageView!
     @IBOutlet var titleLabel: UILabel!
-
+    
     var _isRemoteImage: Bool?
-
+    
     func setupCell(title: String, iconUrl: String, isRemoteImage: Bool) {
-
+        
         _isRemoteImage = isRemoteImage
-
+        
         self.titleLabel.text = title
         self.titleLabel.textColor = UIColor.whiteColor()
-
-
+        
+        
         if (isRemoteImage == true) {
             let url: NSURL = NSURL(string: iconUrl)!
             let image = UIImage(named: "thumbnail_border")
@@ -34,44 +34,44 @@ class MenuCollapseTableCell: UITableViewCell {
             let image = UIImage(named: iconUrl)
             self.iconImage.image = image
         }
-
+        
         // 3
         self.backgroundColor = UIColor.clearColor()
-
-        self.setupLayout()
+        
+//        self.setupLayout()
     }
-
-
+    
+    
     func setupLayout() {
         var width:CGFloat = REAR_VIEW_WIDTH
         var height:CGFloat = TABLE_ROW_HEIGHT
-
+        
         var vTop:CGFloat?
         var vLeft:CGFloat?
         var nodeSize:CGSize?
         var nodeHeight:CGFloat?
-
+        
         var middleY = height / 2
         var icon_WH = LOGIN_ICON_WH
         if(_isRemoteImage == true){
             icon_WH = LOGIN_ICON_WH + 4
         }
-
+        
         //1
         var icon_middle_x = ICON_PADDING_LEFT + LOGIN_ICON_WH/2
-        vTop = (height - icon_WH)/2
+        vTop = middleY - icon_WH/2
         vLeft = icon_middle_x - icon_WH/2
         iconImage?.frame = CGRectMake(vLeft!, vTop!, icon_WH, icon_WH)
-
+        
         //2
         vLeft = ICON_PADDING_LEFT + LOGIN_ICON_WH + ICON_PADDING_RIGHT
         var vWidth = width - vLeft!
-
+        
         nodeHeight = 12
         vTop = middleY - nodeHeight!/2
-
+        
         titleLabel?.frame = CGRectMake(vLeft!, vTop!, vWidth,  nodeHeight!)
     }
-
-
+    
+    
 }
