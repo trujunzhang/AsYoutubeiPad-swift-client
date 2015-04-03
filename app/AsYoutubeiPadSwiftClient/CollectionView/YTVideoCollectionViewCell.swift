5e7da2bd060d489c3ce30f74d71ff4f59420ba45
wanghaogithub720 <wanghaomspace@gmail.com>
2015-04-03 18:35:29 +0800

    xxx

--- app/AsYoutubeiPadSwiftClient/CollectionView/YTVideoCollectionViewCell.swift
+++ app/AsYoutubeiPadSwiftClient/CollectionView/YTVideoCollectionViewCell.swift
@@ -49,9 +49,27 @@ class YTVideoCollectionViewCell: UICollectionViewCell {
         
         // 4
         channelTitleLabel.text = channelTitle
-
+        
+        // 5 
+        setupChannelThumbnail()
     }
     
+    func setupChannelThumbnail(){
+        let channelID = YoutubeParser.getChannelIdByVideo(videoCache)
+        
+        YoutubeFetcher.sharedInstance.fetchChannelForThumbnail(channelID, completeHandler: { (object, sucess) -> Void in
+            if(sucess == true){
+                var array:NSArray = object as NSArray
+ 
+                var channel :MABYT3_Channel = array[0] as MABYT3_Channel
+                var imageUrl = YoutubeModelParser.getMABChannelThumbnalUrl(channel)
+                
+                let url = NSURL(string: imageUrl)
+                self.channelThumbnailImage.hnk_setImageFromURL(url!)
+            }
+        })
+        
+    }
     
     
 }
