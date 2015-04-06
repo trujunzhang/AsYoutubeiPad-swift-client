3e4fc92e6772df8f73e2a26768ec8ba9103b61d5
wanghaogithub720 <wanghaomspace@gmail.com>
2015-04-06 15:21:51 +0800

    xxx

---
+++ app/AsYoutubeiPadSwiftClient/WatchView/Bars/MovieEmbeddedFullscreenBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MovieEmbeddedFullscreenBarViewController.swift
+//  AsYoutubeiPadSwiftClient
+//
+//  Created by djzhang on 4/6/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import Foundation
+
+class MovieEmbeddedFullscreenBarViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
+    }
+}
---
+++ app/AsYoutubeiPadSwiftClient/WatchView/Bars/MovieEmbeddedNormalBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MovieEmbeddedNormalBarViewController.swift
+//  AsYoutubeiPadSwiftClient
+//
+//  Created by djzhang on 4/6/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import Foundation
+
+class MovieEmbeddedNormalBarViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
+    }
+}
