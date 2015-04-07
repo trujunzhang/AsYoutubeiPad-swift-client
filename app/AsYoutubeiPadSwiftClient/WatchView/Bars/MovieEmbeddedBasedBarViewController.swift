e83701d49ef0fcd211c21bd39b897d7978f93f0a
wanghaogithub720 <wanghaomspace@gmail.com>
2015-04-07 16:18:58 +0800

    xxxx

--- app/AsYoutubeiPadSwiftClient/WatchView/Bars/MovieEmbeddedBasedBarViewController.swift
+++ app/AsYoutubeiPadSwiftClient/WatchView/Bars/MovieEmbeddedBasedBarViewController.swift
@@ -18,5 +18,37 @@ class MovieEmbeddedBasedBarViewController :UIViewController {
     
     // MARK : pop animate
     
+    func popAnimationForTopBar(object : AnyObject){
+        let rWidth:CGFloat = -WATCH_BAR_HEIGHT
+        
+        let spring : POPBasicAnimation = POPBasicAnimation(propertyNamed: kPOPViewFrame)
+        
+        
+        let property: POPAnimatableProperty = POPAnimatableProperty.propertyWithName("com.rwt.heightContstraint", initializer: { (object) -> Void in
+            
+            let prop:POPMutableAnimatableProperty = object as POPMutableAnimatableProperty
+            
+            // note the object used is NSLayoutConstraint, the same object we assign the animation to
+            prop.readBlock = {(constraint,values) -> Void in
+                values[0] = constraint.constant
+            }
+            
+            prop.writeBlock = {(object,values) -> Void in
+                let constraint:NSLayoutConstraint = object as NSLayoutConstraint
+                constraint.constant = values[0]
+            }
+            
+            // this helps Pop determine when the animation is over
+            prop.threshold = 0.01
+            
+            
+        }) as POPAnimatableProperty
+        
+        spring.property = property
+        spring.toValue = rWidth
+        
+        object.pop_addAnimation(spring, forKey: "spring")
+    }
+
     
 }
--- app/AsYoutubeiPadSwiftClient/WatchView/Bars/MovieEmbeddedNormalBarViewController.swift
+++ app/AsYoutubeiPadSwiftClient/WatchView/Bars/MovieEmbeddedNormalBarViewController.swift
@@ -68,43 +68,11 @@ class MovieEmbeddedNormalBarViewController: MovieEmbeddedBasedBarViewController 
     func hideTwoBars(){
         self.performClosureAfterDelay(4, block: { () -> Void in
             
-            self.popAnimationForTopBar()
+            self.popAnimationForTopBar(self.topBarTopConstraint)
             
         })
     }
-    
-    func popAnimationForTopBar(){
-        let rWidth:CGFloat = -WATCH_BAR_HEIGHT
-        
-        let spring : POPBasicAnimation = POPBasicAnimation(propertyNamed: kPOPViewFrame)
-
-        
-        let property: POPAnimatableProperty = POPAnimatableProperty.propertyWithName("com.rwt.heightContstraint", initializer: { (object) -> Void in
-            
-            let prop:POPMutableAnimatableProperty = object as POPMutableAnimatableProperty
-            
-            // note the object used is NSLayoutConstraint, the same object we assign the animation to
-            prop.readBlock = {(constraint,values) -> Void in
-                values[0] = constraint.constant
-            }
-            
-            prop.writeBlock = {(object,values) -> Void in
-                let constraint:NSLayoutConstraint = object as NSLayoutConstraint
-                constraint.constant = values[0]
-            }
-            
-            // this helps Pop determine when the animation is over
-            prop.threshold = 0.01
-            
-            
-        }) as POPAnimatableProperty
-        
-        spring.property = property
-        spring.toValue = rWidth
-        
-        self.topBarTopConstraint.pop_addAnimation(spring, forKey: "spring")
-    }
-    
+      
     
     override func viewDidLayoutSubviews() {
         
