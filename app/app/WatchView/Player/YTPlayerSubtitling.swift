8cb90c30edf0d52db92587d49a04da9d0d847182
wanghaogithub720 <wanghaomspace@gmail.com>
2015-04-11 16:03:53 +0800

    xxx

--- app/app/WatchView/Player/YTPlayerSubtitling.swift
+++ app/app/WatchView/Player/YTPlayerSubtitling.swift
@@ -13,28 +13,23 @@ import UIKit
 class YTPlayerSubtitling :PeriodicTimeProtocol {
     var videoID = ""
     
-    var player: AVPlayer? = nil
     var moviePlayerViewController :MoviePlayerViewController? = nil
     
     var subtitlesParts:NSMutableDictionary? = NSMutableDictionary()
     
     var subtitleLabel : UILabel?
     
-    var currentText: NSString = ""
+    var currentText = ""
     
     init(){
         
     }
     
     func fetchTracksAndCaptainForVideo(_videoID: String) {
-        
-
         YoutubeDataFetcher.sharedInstance.fetchCaptainTracksAndCaption(_videoID,completeHandler: { (subtitleString, sucess) -> Void in
             
             if(sucess == true){
-                
                 var subtitle:NSString = subtitleString as! NSString
-                
                 self.prepareSubtitle(subtitle as! String)
             }
             
@@ -43,24 +38,22 @@ class YTPlayerSubtitling :PeriodicTimeProtocol {
     }
     
     func setPlayer(_videoID: String,_subtitleLabel : UILabel ,_moviePlayerViewController: MoviePlayerViewController ){
-//        fetchTracksAndCaptainForVideo(_videoID)
+        fetchTracksAndCaptainForVideo(_videoID)
         
         videoID = _videoID
         subtitleLabel = _subtitleLabel
         moviePlayerViewController = _moviePlayerViewController
-        
-        player = moviePlayerViewController?.videoPlayer
     }
     
     func prepareSubtitle(subtitleString : String){
         
-//        let srtParser:SOSRTParserHelper = SOSRTParserHelper()
-//        
-//        srtParser.parseSRTString(subtitleString, toDictionary: subtitlesParts) { (parsed, error) -> Void in
-//            if(parsed == true){
-//                self.addToPeriodicTimeProtocol()
-//            }
-//        }
+        let srtParser:SOSRTParserHelper = SOSRTParserHelper()
+        
+        srtParser.parseSRTString(subtitleString, toDictionary: subtitlesParts) { (parsed, error) -> Void in
+            if(parsed == true){
+                self.addToPeriodicTimeProtocol()
+            }
+        }
         
     }
     
@@ -69,38 +62,43 @@ class YTPlayerSubtitling :PeriodicTimeProtocol {
     }
     
     func playerTimeChanged(){
-        if let videoPlayer: AVPlayer = player {
-            if let playerItem: AVPlayerItem = videoPlayer.currentItem {
-                let time : CMTime =  playerItem.currentTime()
-                let nbSecondsElapsed: NSTimeInterval = CMTimeGetSeconds(time)
-                
-                
-//                let subtitle:NSString = SRTParserInterface.searchAndShowSubtitle(subtitlesParts, inTime: nbSecondsElapsed)
-                
-//                if (subtitle.isEqualToString(currentText) || subtitle == currentText){
-//                    return
-//                }
-//                currentText = subtitle
-                
-                dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                    // updateLabel
+         if let viewController : MoviePlayerViewController =  moviePlayerViewController {
+            if let videoPlayer: AVPlayer = viewController.videoPlayer {
+                if let playerItem: AVPlayerItem = videoPlayer.currentItem {
+                    let time : CMTime =  playerItem.currentTime()
+                    let nbSecondsElapsed: NSTimeInterval = CMTimeGetSeconds(time)
+                    
+                    
+                    let subtitle:NSString = SRTParserInterface.searchAndShowSubtitle(subtitlesParts, inTime: nbSecondsElapsed)
+                    
+                    if (subtitle.isEqualToString(currentText) || subtitle == currentText){
+                        return
+                    }
+                    currentText = subtitle as String
+                    
+                    //                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    //                    // updateLabel
+                    //                    self.updateLabel()
+                    //                })
+                    
                     self.updateLabel()
-                })
-                
-                println("player time changed : \(currentText)")
+                    
+                    println("player time changed : \(currentText)")
+                }
             }
         }
+
         
         
     }
     
     func updateLabel(){
         
-        subtitleLabel?.text = currentText as? String
+        subtitleLabel?.text = currentText
         
         //        subtitleLabel?.text = "wanghao"
         
-        subtitleLabel?.hidden = currentText.length != 0
+        subtitleLabel?.hidden = currentText.isEmpty == false
     }
     
 }
