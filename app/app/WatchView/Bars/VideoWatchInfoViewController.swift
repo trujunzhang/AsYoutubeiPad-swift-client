e6a5629936435a641e13a0d9281e1f3f07546486
wanghaogithub720 <wanghaomspace@gmail.com>
2015-04-12 10:11:55 +0800

    xxx

---
+++ app/app/WatchView/Bars/VideoWatchInfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  VideoWatchInfoViewController.swift
+//  app
+//
+//  Created by djzhang on 4/12/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import Foundation
+
+
+class VideoWatchInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+    }
+    
+}
---
+++ app/app/WatchView/VideoWatchProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  VideoWatchProtocol.swift
+//  app
+//
+//  Created by djzhang on 4/12/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import Foundation
