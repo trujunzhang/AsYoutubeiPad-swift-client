16751a92ea8a5d915347481c6081b62a1da703d2
wanghaogithub720 <wanghaomspace@gmail.com>
2015-04-14 15:14:10 +0800

    xxx

--- app/app/Cartography/CartoWatchViewController.swift
+++ app/app/Cartography/CartoWatchViewController.swift
@@ -7,25 +7,59 @@
 //
 
 import Foundation
+import Cartography
 
 class CartoWatchViewController : UIViewController {
     
     @IBOutlet weak var moviePlayContainer: UIView!
-   
+    
     @IBOutlet weak var videoInfoContainer: UIView!
     
     @IBOutlet weak var videoOtherContainer: UIView!
     
-    
+    var group:Cartography.ConstraintGroup =  Cartography.ConstraintGroup()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let rect:CGRect = moviePlayContainer.frame
+        println("rect : \(rect)")
+        
+        LayoutUtils.LayoutFullView(self.moviePlayContainer!)
+        
+//         setupViewHorizontalLayout()
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+//        if UIDevice.currentDevice().orientation.isLandscape.boolValue { //landscape
+//            setupViewHorizontalLayout()
+//        } else {// portraight
+//            setupViewVerticalLayout()
+//        }
+    }
+    
+    func setupViewVerticalLayout(){
+        
+    }
+    
+    func setupViewHorizontalLayout(){
+        
+       layout(moviePlayContainer!) { view1 in
+            
+//            view1.leading == view1.superview!.leading
+//            view1.top == view1.superview!.top
+            
+            
+            view1.width   == 576
+            view1.height  == 10
+        }
     }
     
     
     @IBAction func onClickBarButton(sender: AnyObject) {
         var x = 0
     }
-
+    
     
 }
