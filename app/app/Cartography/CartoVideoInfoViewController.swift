72842cac0795660e5eb1e3706ab7a1887994ffde
wanghaogithub720 <wanghaomspace@gmail.com>
2015-04-15 11:00:09 +0800

    xxx

---
+++ app/app/Cartography/CartoVideoInfoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CartoVideoInfoViewController.swift
+//  app
+//
+//  Created by djzhang on 4/15/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import Foundation
+
+class CartoVideoInfoViewController: UIViewController {
+
+    @IBOutlet weak var videoInfoContainer: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
