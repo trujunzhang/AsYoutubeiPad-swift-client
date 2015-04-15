401d6b1ec2a8063e64b3ae2c0dfaa25aea9dc3f0
wanghaogithub720 <wanghaomspace@gmail.com>
2015-04-15 16:18:26 +0800

    xxx

--- app/app/Cartography/CartoVideoInfoViewController.swift
+++ app/app/Cartography/CartoVideoInfoViewController.swift
@@ -36,7 +36,7 @@ class CartoVideoInfoViewController: UIViewController {
     func createview(){
         myscr = UIScrollView()
         //        myscr!.frame = CGRectMake(0,0,300,300)
-        //        myscr!.contentSize = CGSizeMake(300,300)
+                myscr!.contentSize = CGSizeMake(300,300)
         myscr!.scrollEnabled = true
         myscr!.backgroundColor = UIColor.whiteColor()
         
@@ -51,30 +51,45 @@ class CartoVideoInfoViewController: UIViewController {
         
         if let controller : UIViewController = videoInfoViewController{
             
-            //            videoInfoRootView = controller.view
-            
             if let subView : UIScrollView = myscr{
                 subView.addSubview(videoInfoRootView!)
             }
             
-            LayoutDynamicHeight(videoInfoRootView!)
         }
+        
+        LayoutDynamicHeight(videoInfoRootView!)
     }
     
-     func LayoutDynamicHeight(view : UIView){
+    func LayoutDynamicHeight(view : UIView){
         layout(view) { view1 in
             
             view1.top == view1.superview!.top
             view1.leading == view1.superview!.leading
             view1.trailing  == view1.superview!.trailing
             
-                        //            view1.height  == 180
+            //            view1.height  == 180
         }
     }
     
+//    func LayoutForScroller(view : UIView){
+//        layout(view) { view1 in
+//            view1.leadingMargin ==  20
+//            view1.trailingMargin ==  20
+//            view1.topMargin ==  20
+//            view1.bottomMargin ==  20
+//            
+////            view1.leading == view1.superview!.leading + 20
+////            view1.trailing  == view1.superview!.trailing - 20
+////            
+////            view1.top == view1.superview!.top + 20
+////            view1.bottom  == view1.superview!.bottom - 20
+//            
+//        }
+//    }
+    
     override func viewDidLayoutSubviews() {
         if let subView : UIScrollView = myscr{
-            subView.contentSize = videoInfoRootView!.frame.size
+//            subView.contentSize = videoInfoRootView!.frame.size
         }
     }
     
