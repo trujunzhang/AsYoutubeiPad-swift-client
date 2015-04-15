9ed3e8127c3acdb2e819680b79502eaab3a67f7c
wanghaogithub720 <wanghaomspace@gmail.com>
2015-04-15 11:09:40 +0800

    xxx

--- app/app/Cartography/CartoVideoInfoViewController.swift
+++ app/app/Cartography/CartoVideoInfoViewController.swift
@@ -9,12 +9,36 @@
 import Foundation
 
 class CartoVideoInfoViewController: UIViewController {
-
+    
     @IBOutlet weak var videoInfoContainer: UIView!
     
+    var videoInfoViewController:YTVideoWatchInfoViewController?
+    var videoInfoRootView : UIView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        videoInfoViewController = StoryBoardUtils.getYTVideoWatchInfoViewController()
+        
+        if let controller : UIViewController = videoInfoViewController{
+            self.addChildViewController(controller)
+        }
+        
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let controller : UIViewController = videoInfoViewController{
+
+            videoInfoRootView = controller.view
+            
+            self.videoInfoContainer.addSubview(videoInfoRootView!)
+            
+            LayoutUtils.LayoutFullView(videoInfoRootView!)
+        }
+    }
+    
+    
     
 }
