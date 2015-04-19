a844c8c60a20a29f4236a00a009efa43d033c628
wanghaogithub720 <wanghaomspace@gmail.com>
2015-04-19 17:52:57 +0800

    xxx

---
+++ app/app/DetailPage/VideoInfoPanel/VideoInfoConstant.swift
@@ -0,0 +1,20 @@
+//
+//  VideoInfoConstant.swift
+//  app
+//
+//  Created by djzhang on 4/19/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import Foundation
+
+class TableAnimateObject {
+    var maxValue: CGFloat?
+    var minValue: CGFloat?
+    var fromValue: CGFloat?
+    
+    init(_maxValue : CGFloat){
+        maxValue = _maxValue
+    }
+    
+}
---
+++ app/app/DetailPage/VideoInfoPanel/VideoInfoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  VideoInfoViewController.swift
+//  app
+//
+//  Created by djzhang on 4/19/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import Foundation
+
+class VideoInfoViewController :UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    
+}
