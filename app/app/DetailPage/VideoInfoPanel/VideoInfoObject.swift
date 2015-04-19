56f869e385c3542ab8ca72a96c16ad52e3dbf193
wanghaogithub720 <wanghaomspace@gmail.com>
2015-04-19 17:59:45 +0800

    xxx

--- app/app/DetailPage/VideoInfoPanel/VideoInfoObject.swift
+++ app/app/DetailPage/VideoInfoPanel/VideoInfoObject.swift
@@ -10,6 +10,8 @@ import Foundation
 
 class VideoInfoObject {
     
-    var descriptionString = ""
+    var title = "WebStorm 10 - New features"
+    var likeCount = "2,222 views"
+    var descriptionString = "Both -updateConstraints and -updateViewConstraints may be called multiple times during a view's lifetime. (Calling setNeedsUpdateConstraints on a view will trigger this to happen, for example.) As a result, you need to make sure to prevent creating and activating duplicate constraints -- either using a BOOL to only perform certain constraint setup only once, or by making sure to deactivate/remove existing constraints before creating & activating new ones."    
     
 }
--- app/app/DetailPage/VideoInfoPanel/VideoInfoViewController.swift
+++ app/app/DetailPage/VideoInfoPanel/VideoInfoViewController.swift
@@ -9,12 +9,31 @@
 import Foundation
 
 class VideoInfoViewController :UIViewController {
+
+    var model: NITableViewModel?
+    var tableView : UITableView?
+    var open:Bool = false
+    var cellFactory : NICellFactory?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+    }
+    
+    // MARK : model
+    func makeModel(){
+        let  drawTextBlock:NICellDrawRectBlock = {
+             rect,  object,  cell in
+            
+            
+            return 0
+        }
+        
+        
     }
     
     
