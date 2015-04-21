bcf4af9539c700f937ac23933bbb2319924e5bf7
wanghaogithub720 <wanghaomspace@gmail.com>
2015-04-21 18:50:44 +0800

    xxx

---
+++ app/app/RevealViewControllers/leftmenu/LeftMenuConstant.swift
@@ -0,0 +1,12 @@
+//
+//  LeftMenuConstant.swift
+//  app
+//
+//  Created by djzhang on 4/21/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import Foundation
+
+
+let LEFT_MENU_WIDTH: CGFloat = 240
