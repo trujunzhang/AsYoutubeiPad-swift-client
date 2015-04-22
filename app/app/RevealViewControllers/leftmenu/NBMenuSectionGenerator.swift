b9de2a004180f82a24e3b6e1b5238d1aaaa3b268
wanghaogithub720 <wanghaomspace@gmail.com>
2015-04-22 09:32:49 +0800

    dev videoInfoPanel animate by pop.

--- app/app/RevealViewControllers/leftmenu/NBMenuSectionGenerator.swift
+++ app/app/RevealViewControllers/leftmenu/NBMenuSectionGenerator.swift
@@ -10,5 +10,7 @@ import Foundation
 
 
 class NBMenuSectionGenerator {
-    
+
+
+
 }
--- app/app/RevealViewControllers/leftmenu/NBMenuTableViewController.swift
+++ app/app/RevealViewControllers/leftmenu/NBMenuTableViewController.swift
@@ -10,90 +10,84 @@ import Foundation
 import UIKit
 import Cartography
 
-class NBMenuTableViewController: UIViewController,UITableViewDelegate {
-    
+class NBMenuTableViewController: UIViewController, UITableViewDelegate {
+
     var model: NIMutableTableViewModel?
     var tableView: UITableView?
     var cellFactory: NICellFactory?
-    
+
     var videoInfoObject: VideoInfoObject?
     var obj: NIDrawRectBlockCellObject?
-    
+
     var tableContents: [AnyObject] = [AnyObject]()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.view.backgroundColor = UIColor(rgba: VIDEO_INFO_BACKGROUND_COLOR)
-        
+
+
         makeModel()
-        
+
         tableView = UITableView()
-        
+        tableView!.backgroundColor = UIColor.blackColor()
+
         self.view.addSubview(tableView!)
-        
+
         layout(tableView!) {
             view1 in
-            
+
             view1.leading == view1.superview!.leading
 
             view1.top == view1.superview!.top
             view1.bottom == view1.superview!.bottom
 
-            view1.width ==  LEFT_MENU_WIDTH
+            view1.width == LEFT_MENU_WIDTH
         }
-        
+
         tableView?.dataSource = model
         tableView?.delegate = self
     }
 
-    
+
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
     }
-    
+
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
     }
-    
-    
-    
+
+
+
     // MARK : model
     func makeModel() {
         let drawTextBlock: NICellDrawRectBlock = {
             rect, object, cell in
-            
+
             return 0
         }
-        
+
         videoInfoObject = VideoInfoObject()
         obj = NIDrawRectBlockCellObject(block: drawTextBlock, object: videoInfoObject)
         tableContents = [
-            NIDrawRectBlockCellObject(block: drawTextBlock, object: videoInfoObject),
-            NITitleCellObject(title: "toggle"),
-            NITitleCellObject(title: "xcode"),
-            NITitleCellObject(title: "wanghao")
+                NIDrawRectBlockCellObject(block: drawTextBlock, object: videoInfoObject),
+                NITitleCellObject(title: "toggle", image: UIImage(named: "Film.png")),
+                NITitleCellObject(title: "xcode"),
+                NITitleCellObject(title: "wanghao")
         ]
-        
+
         cellFactory = NICellFactory()
         cellFactory?.mapObjectClass(NIDrawRectBlockCellObject.self, toCellClass: VideoInfoDrawRectBlockCell.self)
-        
+
         model = NIMutableTableViewModel(listArray: tableContents, delegate: cellFactory)
     }
-    
+
     // MARK: UITableViewDelegate
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        if (indexPath.section == 0 && indexPath.row == 0) {
-            if let infoObject: VideoInfoObject = videoInfoObject {
-                let height = infoObject.currentRowHeight + VIDEO_INFO_TITLE_PANEL_HEIGHT
-                //                println("tablerow's height: \(height)")
-                return height
-            }
-        }
-        
         return 200
     }
-    
+
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         if (indexPath.section == 0 && indexPath.row == 1) {
             //            performAnimation()
@@ -101,6 +95,4 @@ class NBMenuTableViewController: UIViewController,UITableViewDelegate {
     }
 
 
-    
-    
 }
