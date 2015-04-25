80b9b6c36a0358ad39158649d64641df002a3cfe
wanghaogithub720 <wanghaobackup@gmail.com>
2015-04-25 20:53:47 +0800

    dev leftmenu.

--- app/app/RevealViewControllers/leftmenu/LeftMenuViewController.swift
+++ app/app/RevealViewControllers/leftmenu/LeftMenuViewController.swift
@@ -9,43 +9,36 @@
 import UIKit
 import Cartography
 
-class LeftMenuViewController: UIViewController ,SWRevealViewControllerDelegate{
-    
+class LeftMenuViewController: UIViewController, SWRevealViewControllerDelegate {
+
     var menuTableViewController: NBMenuTableViewController?
-    var menuTableRootView: UIView?
-    
-    
+
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Do any additional setup after loading the view.
-        
         initMenuTableView()
-        
-        
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     func initMenuTableView() {
         menuTableViewController = NBMenuTableViewController.instance()
-        
-        if let viewController:UIViewController = menuTableViewController {
-            menuTableRootView = viewController.view
-            
-            self.view.addSubview(menuTableRootView!)
+
+        if let viewController: UIViewController = menuTableViewController {
+            let menuTableRootView: UIView = viewController.view
+
+            self.view.addSubview(menuTableRootView)
             self.addChildViewController(viewController)
         }
     }
-    
-    
-    
-    
+
+
 }
