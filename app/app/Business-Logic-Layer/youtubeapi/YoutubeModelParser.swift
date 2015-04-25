e4574fbd7e034e7999787c360edddf0301f474f4
wanghaogithub720 <wanghaobackup@gmail.com>
2015-04-25 18:45:43 +0800

    dev leftmenu.

--- app/app/Business-Logic-Layer/youtubeapi/YoutubeModelParser.swift
+++ app/app/Business-Logic-Layer/youtubeapi/YoutubeModelParser.swift
@@ -10,79 +10,79 @@ import Foundation
 
 
 class YoutubeModelParser {
-    
+
     // MARK: Parse for MABYT3_Channel
     class func getMABChannelThumbnalUrl(channel: MABYT3_Channel) -> NSString {
-        var thumbnail:MABYT3_Thumbnail =  channel.snippet.thumbnails["default"] as! MABYT3_Thumbnail
+        var thumbnail: MABYT3_Thumbnail = channel.snippet.thumbnails["default"] as! MABYT3_Thumbnail
         return thumbnail.url!
     }
-    
+
     // MARK: Parse for GTLYouTubeVideo
-    
+
     class func getVideoSnippetThumbnails(video: YoutubeVideoCache) -> NSString {
         return video.snippet.thumbnails.standard.url;
     }
-    
+
     class func getWatchVideoId(video: GTLYouTubeVideo) -> NSString {
         return video.identifier;
     }
-    
+
     class func getChannelIdByVideo(video: GTLYouTubeVideo) -> NSString {
         return video.snippet.channelId;
     }
-    
+
     class func getVideoSnippetTitle(video: GTLYouTubeVideo) -> NSString {
         return video.snippet.title;
     }
-    
-    
+
+
     class func getVideoSnippetChannelTitle(video: GTLYouTubeVideo) -> NSString {
         return video.snippet.channelTitle;
     }
-    
-    
+
+
     // MARK: Parse for GTLYouTubeChannel
     class func getAuthChannelSnippetThumbnailUrl(channel: GTLYouTubeChannel) -> NSString {
         //        return channel.snippet.thumbnails.high.url
         var stand: GTLYouTubeThumbnail = channel.snippet.thumbnails.standard
-        
+
         return stand.url
     }
-    
+
     class func getAuthChannelTitle(channel: GTLYouTubeChannel) -> NSString {
         return channel.snippet.title
     }
-    
+
     class func getAuthChannelID(channel: GTLYouTubeChannel) -> NSString {
         return channel.identifier
     }
-    
-    
+
+
     // MARK: Parse for GTLYouTubeSubscription
     class func getSubscriptionTitle(subscription: GTLYouTubeSubscription) -> NSString {
         return subscription.snippet.title
     }
-    
+
     class func getSubscriptionUrl(subscription: GTLYouTubeSubscription) -> NSString {
         var thumbnails: GTLYouTubeThumbnailDetails = subscription.snippet.thumbnails
         var dictionary: NSMutableDictionary = thumbnails.JSON["default"] as! NSMutableDictionary
         var defaultValue: NSString = dictionary["url"] as! NSString
-        
+
         return defaultValue
     }
-    
-//    class func convertToMenuRowArrayFromSubscriptions(subscriptions: NSArray) -> [MenuRowItemInfo] {
-//        // array of MenuRowItemInfo
-//        var rows = [MenuRowItemInfo]()
-//        
-//        for subscription in subscriptions {
-//            var title = self.getSubscriptionTitle(subscription as! GTLYouTubeSubscription)
-//            var url = self.getSubscriptionUrl(subscription as! GTLYouTubeSubscription)
-//            var rowItem: MenuRowItemInfo =
-//            MenuRowItemInfo(title: title as String, imageUrl: url as String, rowHParas: YTPlayListItemsType.kUploadsTag.rawValue)
-//            rows.append(rowItem)
-//        }
-//        return rows
-//    }
-    
+
+    class func convertToMenuRowArrayFromSubscriptions(subscriptions: NSArray) -> [MenuRowItemInfo] {
+        // array of MenuRowItemInfo
+        var rows = [MenuRowItemInfo]()
+
+        for subscription in subscriptions {
+            var title: String = self.getSubscriptionTitle(subscription as! GTLYouTubeSubscription) as String
+            var url: String = self.getSubscriptionUrl(subscription as! GTLYouTubeSubscription) as String
+            var rowItem: MenuRowItemInfo =
+            MenuRowItemInfo(title: title, imageUrl: url, rowHParas: YTPlayListItemsType.kUploadsTag.rawValue)
+            rows.append(rowItem)
+        }
+        return rows
+    }
+
 }
