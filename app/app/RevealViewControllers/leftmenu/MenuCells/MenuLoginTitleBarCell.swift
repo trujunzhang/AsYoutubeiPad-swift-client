2e3a0e110b4224496e463b94562d78e76f10b879
wanghaogithub720 <wanghaobackup@gmail.com>
2015-04-26 16:02:46 +0800

    dev leftmenu.

--- app/app/RevealViewControllers/leftmenu/MenuCells/MenuLoginTitleBarCell.swift
+++ app/app/RevealViewControllers/leftmenu/MenuCells/MenuLoginTitleBarCell.swift
@@ -15,76 +15,73 @@ import Cartography
 
 class MenuLoginTitleBarCellObject: NICellObject {
     var menuTitleBarTapProtocol: NBMenuTitleBarTapProtocol?
-
+    
     init(menuTitleBarTapProtocol: NBMenuTitleBarTapProtocol) {
         super.init(cellClass: MenuLoginTitleBarCell.self, userInfo: nil)
-
+        
         self.menuTitleBarTapProtocol = menuTitleBarTapProtocol
     }
 }
 
 class MenuLoginTitleBarCell: MenuTitleBarBaseCell, NICell {
     var loginLabel: UILabel?
-
+    
     // MARK : Life Cycle
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-
+        
         loginLabel = UILabel()
         if let theLoginLabel: UILabel = loginLabel {
             theLoginLabel.textColor = UIColor.whiteColor()
-            theLoginLabel.text = "Login in"
-
+            
             self.addSubview(theLoginLabel)
         }
-
+        
         if let theLoginLabel: UILabel = loginLabel {
-
+            
             layout(theLoginLabel) {
                 view2 in
-
-//                // theUserName
-//                view2.leadingMargin == 40 + 6
-//                view2.top == view2.superview!.top + 6
-//
-//                view2.width == 140
-//                view2.height == 14
-//
-//                // theEmailLabel
-//                view3.leading == 40 + 6
-//                view3.top == view2.bottom + 6
-//
-//                view3.width == 140
-//                view3.height == 14
+                
+                // theUserName
+                view2.leading == view2.superview!.leading + 50
+                view2.centerY == view2.superview!.centerY
+                
+                view2.width == 140
+                view2.height == 14
+                
             }
         }
-
-
+        
+        
     }
-
+    
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-
+    
     override func prepareForReuse() {
         super.prepareForReuse()
-
+        
         self.loginLabel!.text = nil
     }
-
+    
     func shouldUpdateCellWithObject(object: AnyObject!) -> Bool {
         backgroundColor = UIColor.clearColor()
-
+        
         let infoObject: MenuLoginTitleBarCellObject = object as! MenuLoginTitleBarCellObject
         self.menuTitleBarTapProtocol = infoObject.menuTitleBarTapProtocol
-
+        
+        if let theLoginLabel: UILabel = loginLabel {
+            theLoginLabel.text = "Login in"
+        }
+        
         return true
     }
-
+    
     class func shouldAppendObjectClassToReuseIdentifier() -> Bool {
         return true
     }
-
+    
 }
 
 
