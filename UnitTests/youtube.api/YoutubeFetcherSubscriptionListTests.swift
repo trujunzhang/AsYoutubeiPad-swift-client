2124ad0ededce29562cec333782624191d331558
wanghaogithub720 <wanghaobackup@gmail.com>
2015-04-28 10:48:44 +0800

    xxx

---
+++ UnitTests/youtube.api/YoutubeFetcherSubscriptionListTests.swift
@@ -0,0 +1,9 @@
+//
+//  YoutubeFetcherSubscriptionListTests.swift
+//  appRestApp
+//
+//  Created by djzhang on 4/28/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import Foundation
