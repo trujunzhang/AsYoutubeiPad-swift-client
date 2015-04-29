0edefcf7f984534ff2358d5b47586028c89a35ea
wanghaogithub720 <wanghaobackup@gmail.com>
2015-04-29 17:04:17 +0800

    dev newSubscriptionsList.

--- app/app/CollectionView/YTVideosCollectionViewController.swift
+++ app/app/CollectionView/YTVideosCollectionViewController.swift
@@ -9,69 +9,87 @@
 import Foundation
 
 class YTVideosCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
-    
+
     @IBOutlet var collectionView: UICollectionView!
-    
+
     var requestInfo = YTYoutubeRequestInfo()
 
-    
-    func makeRequestTask(){
-        requestInfo = YoutubeFetcher.sharedInstance.prepareRequestSearch("Sketch 3", completeHandler: { (object, sucess) -> Void in
-            if(sucess == true){
-                var array:NSArray = object as! NSArray
-                
+
+    func makeRequestTask() {
+        requestInfo = YoutubeFetcher.sharedInstance.prepareFetchingActivityListOnHomePage(  {
+            (object, sucess) -> Void in
+            if (sucess == true) {
+                var array: NSArray = object as! NSArray
+
                 var length = array.count
-                
+
+                println("Length is \(length)")
+
                 self.requestInfo.appendArray(array)
-                
+
                 self.collectionView.reloadData()
             }
         })
     }
-    
-    
+
+    func makeRequestTask123() {
+        requestInfo = YoutubeFetcher.sharedInstance.prepareRequestSearch("Sketch 3", completeHandler: {
+            (object, sucess) -> Void in
+            if (sucess == true) {
+                var array: NSArray = object as! NSArray
+
+                var length = array.count
+
+                self.requestInfo.appendArray(array)
+
+                self.collectionView.reloadData()
+            }
+        })
+    }
+
+
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
+
     // Mark :
     override func viewDidLoad() {
         super.viewDidLoad()
         self.collectionView.dataSource = self
         self.collectionView.delegate = self
-        
+
 //        self.view.backgroundColor = UIColor.yellowColor()
 //        self.collectionView.backgroundColor = UIColor.orangeColor()
-        
+
         makeRequestTask()
     }
-    
+
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
 
     }
-    
+
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
     }
-    
+
     // Mark : delegate of UICollectionViewDataSource
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.requestInfo.getVideoListCount()
     }
-    
+
     // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell:YTVideoCollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("YTVideoCollectionViewCell", forIndexPath: indexPath)as! YTVideoCollectionViewCell
-        
-        let videoCache:YoutubeVideoCache = self.requestInfo.videoList[indexPath.row] as! YoutubeVideoCache
+        let cell: YTVideoCollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("YTVideoCollectionViewCell", forIndexPath: indexPath) as! YTVideoCollectionViewCell
+
+        let videoCache: YoutubeVideoCache = self.requestInfo.videoList[indexPath.row] as! YoutubeVideoCache
         cell.setupCell(videoCache)
-        
+
         return cell
     }
-    
-    
-    
+
+
+
     // Mark : delegate of UICollectionViewDelegate
-    
+
 }
