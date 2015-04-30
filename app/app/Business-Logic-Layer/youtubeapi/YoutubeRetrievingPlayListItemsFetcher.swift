ef1a9406f162f4f2b1897452286c278496c179f4
wanghaogithub720 <wanghaobackup@gmail.com>
2015-04-30 10:55:00 +0800

    Retrieving all the new subscription videos in YouTube v3 API.

--- app/app/Business-Logic-Layer/youtubeapi/YoutubeRetrievingPlayListItemsFetcher.swift
+++ app/app/Business-Logic-Layer/youtubeapi/YoutubeRetrievingPlayListItemsFetcher.swift
@@ -12,7 +12,25 @@ import Foundation
 class YoutubeRetrievingPlayListItemsFetcher: NSObject {
     var delegate: FetchingNextDelegate?
 
+    var parameters: [String:String] = [
+            "part": "snippet,contentDetails",
+            "fields": "items/contentDetails,items/snippet(publishedAt,title)",
+            "maxResults": "5",
+    ]
+
+    override init() {
+
+    }
+
     func fetchingNextPlayListItemsFromChannelList(playlistID: NSString) {
+        parameters["playlistId"] = playlistID as String
+//        var parameters = NSMutableDictionary(dictionary: [
+//                "part": "snippet,contentDetails",
+//                "fields": "items/contentDetails,items/snippet(publishedAt,title)",
+//                "maxResults": "5",
+//                "playlistId": playlistID,
+//        ]
+//        )
 
         YoutubeFetcher.sharedInstance.fetchPlayListItemWithPlayListId(playlistID, completeHandler: {
             (object, sucess) -> Void in
