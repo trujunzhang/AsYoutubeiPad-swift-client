7ba14827df28e29b5d64534ffae35d7403174898
wanghaogithub720 <wanghaobackup@gmail.com>
2015-04-30 19:28:28 +0800

    dev collection by nimbus's collectionMultipleModel.

---
+++ app/app/RevealViewControllers/Utils/RevealViewHelper.swift
@@ -0,0 +1,16 @@
+//
+//  RevealViewHelper.swift
+//  app
+//
+//  Created by djzhang on 4/30/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import Foundation
+
+class RevealViewHelper: NSObject {
+    var subscriptionsViewController:SubscriptionsViewController?
+    
+
+    
+}
