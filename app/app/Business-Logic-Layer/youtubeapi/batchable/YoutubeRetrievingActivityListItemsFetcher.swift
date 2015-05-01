4e8d54c074b111e7931cd91dc954e60fe296e6db
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-01 18:00:06 +0800

    Retrieving all the newest videos by fetching activityList  in YouTube v3 API.

--- app/app/Business-Logic-Layer/youtubeapi/batchable/YoutubeRetrievingActivityListItemsFetcher.swift
+++ app/app/Business-Logic-Layer/youtubeapi/batchable/YoutubeRetrievingActivityListItemsFetcher.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+
+class YoutubeRetrievingActivityListItemsFetcher: NSObject {
+    var delegate: FetchingNextDelegate?
+
+    var parameters: NSMutableDictionary = [
+            "part": "snippet,contentDetails",
+//            "fields": "items/contentDetails,items/snippet(publishedAt,title)",
+            "fields": "items/contentDetails,items/snippet(publishedAt)",
+            "maxResults": "2",
+            "key": apiKey
+    ]
+
+    override init() {
+
+    }
+
+    func fetchingNextActivityListItemsFromChannelList(playlistID: NSString) {
+        parameters["playlistId"] = playlistID as String
+
+        YoutubeFetcher.fetchActivityListWithoutAuth(parameters, completeHandler: {
+            (object, sucess) -> Void in
+
+            if (sucess == true) {
+                var array: NSArray = object as! NSArray
+
+                if (self.delegate != nil) {
+                    self.delegate!.nextFetching(array)
+                }
+            }
+        })
+
+    }
+}
--- app/app/Business-Logic-Layer/youtubeapi/batchable/YoutubeRetrievingActivityListItemsFetcherHelper.swift
+++ app/app/Business-Logic-Layer/youtubeapi/batchable/YoutubeRetrievingActivityListItemsFetcherHelper.swift
@@ -7,3 +7,59 @@
 //
 
 import Foundation
+
+
+protocol RetrievingActivityListItemsFetchingHelperDelegate {
+//    func failureFetchingNewsSubscriptionVideos()
+
+    func endFetchingActivityListItems(array: NSArray)
+}
+
+class YoutubeRetrievingActivityListItemsFetcherHelper: NSObject, FetchingNextDelegate {
+    var delegate: RetrievingActivityListItemsFetchingHelperDelegate?
+    var playlistItemsIdsArray: NSArray?
+    var fetchingStep: Int = 0
+
+    var fetchedActivityListItems: NSMutableArray = NSMutableArray()
+    var youtubeRetrievingActivityListItemsFetcher: YoutubeRetrievingActivityListItemsFetcher?
+
+    func startFetchingplaylistItems(playlistItemsIdsArray: NSArray) {
+        self.playlistItemsIdsArray = playlistItemsIdsArray
+
+        youtubeRetrievingActivityListItemsFetcher = YoutubeRetrievingActivityListItemsFetcher()
+        if let playListItemsFetcher: YoutubeRetrievingActivityListItemsFetcher = youtubeRetrievingActivityListItemsFetcher {
+            playListItemsFetcher.delegate = self
+        }
+
+        fetchingNextStep()
+    }
+
+    func fetchingNextStep() {
+        if (fetchingStep == playlistItemsIdsArray!.count) {
+            if (self.delegate != nil) {
+                self.delegate!.endFetchingActivityListItems(fetchedActivityListItems)
+            }
+            return
+        }
+
+        let object: AnyObject? = self.playlistItemsIdsArray?.objectAtIndex(fetchingStep)
+        var playlistID: NSString = object as! NSString
+        fetchingStep++
+
+        if let playListItemsFetcher: YoutubeRetrievingActivityListItemsFetcher = youtubeRetrievingActivityListItemsFetcher {
+            playListItemsFetcher.fetchingNextActivityListItemsFromChannelList(playlistID)
+        }
+    }
+
+
+
+    // MARK: FetchingNextDelegate
+    func nextFetching(array: NSObject) {
+        let playListItems = array as! [AnyObject]
+        fetchedActivityListItems.addObjectsFromArray(playListItems)
+
+//        println("Result in nextFetching is \(playListItems.count)")
+
+        fetchingNextStep()
+    }
+}
