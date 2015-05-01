738e0834d0120f0e8c1373a6ca476bddabd415a2
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-01 20:12:59 +0800

    Retrieving all the newest videos by fetching activityList  in YouTube v3 API.

--- UnitTests/youtube.api/YoutubeFetchActivityListTests.swift
+++ UnitTests/youtube.api/YoutubeFetchActivityListTests.swift
@@ -55,7 +55,7 @@ class YoutubeFetchActivityListTests: YoutubeFetcherBase, RetrievingItemsFetching
         let sortedArray = YoutubeParser.sortNewestVideoListInActivityList(array)
         let newestVideoIdsArray = YoutubeParser.getNewestVideoIdsArrayByMABActivityList(sortedArray)
 
-        println("count of newestVideoIdsArray is \(newestVideoIdsArray.count)")
+        println("count of sortedArray is \(sortedArray.count)")
         println("newestVideoIdsArray is \(newestVideoIdsArray)")
 
         expectation!.fulfill()
