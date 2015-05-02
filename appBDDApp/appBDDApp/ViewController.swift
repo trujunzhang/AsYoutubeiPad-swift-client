6abdba2c56b011778d650f793829cee608456ef1
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-02 11:45:48 +0800

    xxx

--- appBDDApp/appBDDApp/ViewController.swift
+++ appBDDApp/appBDDApp/ViewController.swift
@@ -1,18 +1,26 @@
 //
 //  ViewController.swift
-//  appBDDApp
+//  appRestApp
 //
-//  Created by djzhang on 5/1/15.
+//  Created by djzhang on 4/24/15.
 //  Copyright (c) 2015 djzhang. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AuthorUserFetchingDelegate {
 
+    @IBOutlet weak var loginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        AuthoredFetcher.sharedInstance.delegate = self
+        if (YoutubeUserProfile.sharedInstance.hasLogin() == true) {
+            loginButton.enabled = false
+            loginButton.backgroundColor = UIColor.redColor()
+
+//            self.startFetchingLoggedSubscriptionList() // used
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +29,50 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func loginTapped(sender: AnyObject) {
+        let viewController: GTMOAuth2ViewControllerTouch =
+        GTMOAuth2ViewControllerTouch(scope: scope, clientID: kMyClientID, clientSecret: kMyClientSecret, keychainItemName: keychainItemName) {
+            (controllerTouch, auth, error) -> Void in
+
+            if (error != nil) {
+                // Authentication failed
+
+            } else {
+                // Authentication succeeded
+                YoutubeUserProfile.sharedInstance.authorizeRequest(auth)
+                self.startFetchingLoggedSubscriptionList()
+                self.dismissViewControllerAnimated(true, completion: {
+                    () -> Void in
+
+                })
+            }
+        }
+
+        let navCon: UINavigationController = UINavigationController(rootViewController: viewController)
+        navCon.providesPresentationContextTransitionStyle = true
+        navCon.definesPresentationContext = true
+        navCon.modalPresentationStyle = .PageSheet
+
+
+        self.presentViewController(navCon, animated: true) {
+            () -> Void in
+
+        }
+    }
+
+    func startFetchingLoggedSubscriptionList() {
+        AuthoredFetcher.sharedInstance.initLoggedUser()
+    }
+
+    // MARK: AuthorUserFetchingDelegate
+    func endFetchingUserChannel(channel: GTLYouTubeChannel) {
+        let x = 0
+
+    }
+
+    func endFetchingUserSubscriptions(array: NSArray) {
+        let x = 0
+
+    }
 }
 
