f77818b2b174f299bb7f06aa30e3c04d2e0b32c6
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-02 19:53:40 +0800

    xxx

---
+++ app/app/CollectionView/FetchEvents/FetchEventProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  FetchEventProtocol.swift
+//  app
+//
+//  Created by djzhang on 5/2/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import Foundation
+
+protocol FetchEventProtocol {
+    
+}
