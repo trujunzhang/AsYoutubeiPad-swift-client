322c931223fef60bc3081259af8688eaa188c1d6
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-02 17:41:36 +0800

    xxx

--- Specs/SwiftyUserDefaults/SwiftyUserDefaultsSpec.swift
+++ Specs/SwiftyUserDefaults/SwiftyUserDefaultsSpec.swift
@@ -3,6 +3,12 @@ import Nimble
 
 class SwiftyUserDefaultsSpec: QuickSpec {
     override func spec() {
-        
+        describe("the 'Documentation' directory") {
+            it("is peel") {
+                let banana: Banana = Banana()
+                banana.peel()
+                expect(banana.isEdible).to(beTruthy())
+            }
+        }
     }
 }
