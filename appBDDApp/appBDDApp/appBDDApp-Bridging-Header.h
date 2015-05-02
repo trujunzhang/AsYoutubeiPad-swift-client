758fbcec4f73049543cec7cb8e6f464409c41eb7
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-02 11:46:04 +0800

    xxx

---
+++ appBDDApp/appBDDApp/appBDDApp-Bridging-Header.h
@@ -0,0 +1,33 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+// Project Object-c files
+
+// Library: gtm-oauth2
+#import "GTMOAuth2Authentication.h"
+#import "GTMOAuth2ViewControllerTouch.h"
+
+// Library : google-api-client[Youtube]
+#import "GTLYouTube.h"
+#import "GTLService.h"
+
+// Library: YouTubeAPI3-Objective-C-wrapper
+#import "MABYouTube_Sources.h"
+#import "YoutubeParser.h"
+#import "GTLYoutubeUtils.h"
+
+// Library: IGYouTubeExtractor
+#import "IGYouTubeExtractor.h"
+
+// Library: Subtitles [used]
+//#import "SOSubtitle.h"
+//#import "SOSRTParserHelper.h"
+//#import "SRTParserInterface.h"
+
+// Library: Bolts-iOS
+//#import <Bolts/Bolts.h>
+
+
+#import <AFNetworking/AFNetworking.h>
