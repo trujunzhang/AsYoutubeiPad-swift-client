2f0669eed80f22ec50100a578d19ca384309b283
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-03 13:56:03 +0800

    xxx

--- app/app/CollectionView/YTVideosCollectionViewController.swift
+++ app/app/CollectionView/YTVideosCollectionViewController.swift
@@ -9,15 +9,15 @@
 import Foundation
 
 class YTVideosCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
-    
+
     @IBOutlet var collectionView: UICollectionView!
-    
+
     @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
-    
+
     var delegate: FetchEventProtocol?
-    
+
     var videoList: NSMutableArray = NSMutableArray()
-    
+
     //    func makeRequestTask123() {
     //      let  requestInfo = AuthoredFetcher.sharedInstance.prepareFetchingActivityListOnHomePage({
     //            (object, sucess) -> Void in
@@ -34,7 +34,7 @@ class YTVideosCollectionViewController: UIViewController, UICollectionViewDataSo
     //            }
     //        })
     //    }
-    
+
     //    func makeRequestTask() {
     //        requestInfo = YoutubeFetcher.prepareRequestSearch("Sketch 3", completeHandler: {
     //            (object, sucess) -> Void in
@@ -49,123 +49,118 @@ class YTVideosCollectionViewController: UIViewController, UICollectionViewDataSo
     //            }
     //        })
     //    }
-    
-    
+
+
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
+
     // Mark :
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.flowLayout.itemSize = CGSizeMake(214, 200)
         self.collectionView.dataSource = self
         self.collectionView.delegate = self
-        
+
         if let theCollectionView: UICollectionView = self.collectionView {
             theCollectionView.contentInset = UIEdgeInsetsMake(20.0, 20.0, 20.0, 20.0)
             theCollectionView.backgroundColor = UIColor.clearColor()
-            
-            theCollectionView.addInfiniteScrollWithHandler({ (collectionView) -> Void in
+
+            theCollectionView.addInfiniteScrollWithHandler({
+                (collectionView) -> Void in
                 self.insertRowAtBottom()
             })
-            
-            //            theCollectionView.addInfiniteScrollingWithActionHandler({
-            //                [weak self] in
-            //                if let actualSelf = self {
-            //                    // Captures an owning reference "actualSelf" so interacting with it in this
-            //                    // block is safe
-            //                    actualSelf.insertRowAtBottom()
-            //                }
-            //            })
         }
+
+        insertRowAtTop()
     }
-    
+
     func insertRowAtTop() {
         self.cleanupFetchedArray()
-        
+
         self.delegate!.refreshEvent({
             (response, sucess) -> Void in
-            
-            self.stopFetchedAnimation()
-            
+
             self.appendFetchedArray(response as! NSArray)
         })
     }
-    
+
     func insertRowAtBottom() {
-        if (self.delegate!.hasNextFetcing() == false) {
-            self.stopFetchedAnimation()
-            return
-        }
         self.delegate!.nextFetching({
             (response, sucess) -> Void in
-            
-            self.stopFetchedAnimation()
-            
+
             self.appendFetchedArray(response as! NSArray)
         })
     }
-    
+
     func cleanupFetchedArray() {
         self.videoList = NSMutableArray()
         self.collectionView.reloadData()
     }
-    
+
     func appendFetchedArray(array: NSArray) {
+        // 1. stop animation
+        self.stopFetchedAnimation()
+
+        // 2. update collection view
         self.videoList.addObjectsFromArray(array as [AnyObject])
         self.collectionView.reloadData()
+
+        // 3. check next fetcher
+        if (self.delegate!.hasNextFetcing() == false) {
+            self.collectionView.removeInfiniteScroll()
+        }
     }
-    
+
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        
+
     }
-    
+
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        
+
         self.startFetchedAnimation()
     }
-    
+
     func startFetchedAnimation() {
         if let theCollectionView: UICollectionView = self.collectionView {
             //            theCollectionView.triggerPullToRefresh()
         }
     }
-    
+
     func stopFetchedAnimation() {
         if let theCollectionView: UICollectionView = self.collectionView {
             theCollectionView.finishInfiniteScroll()
         }
     }
-    
-    
+
+
     // Mark : delegate of UICollectionViewDataSource
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return videoList.count
     }
-    
+
     // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell: YTVideoCollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("YTVideoCollectionViewCell", forIndexPath: indexPath) as! YTVideoCollectionViewCell
-        
+
         let videoCache: YoutubeVideoCache = videoList[indexPath.row] as! YoutubeVideoCache
         cell.setupCell(videoCache)
-        
+
         return cell
     }
-    
-    
-    
+
+
+
     //MARK: UICollectionViewDelegate
-    
+
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         let videoCache: YoutubeVideoCache = videoList[indexPath.row] as! YoutubeVideoCache
         let videoId: NSString = YoutubeParser.getWatchVideoId(videoCache)
-        
+
         RevealViewHelper.sharedInstance.pushWatchVideoViewController(videoId)
     }
-    
+
 }
