b3822e551ff6e13188ad7b2c80237e432d6c9278
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-04 14:06:45 +0800

    add UIScrollView-InfiniteScroll.

--- app/app/CollectionView/UICollectionCells/VideoCollectionViewCellLayout.swift
+++ app/app/CollectionView/UICollectionCells/VideoCollectionViewCellLayout.swift
@@ -7,19 +7,42 @@
 //
 
 import Foundation
+import Cartography
+
 
 class VideoCollectionViewCellLayout {
+    var thumbnailImage: UIImageView!
     var titleLabel: UILabel!
     var infoLabel: UILabel!
-    var thumbnailImage: UIImageView!
     var channelContainer: UIView!
     var channelThumbnailImage: UIImageView!
     var channelTitleLabel: UILabel!
 
-    class func layoutVideoCollectionViewCell(titleLabel: UILabel, infoLabel: UILabel, thumbnailImage: UIImageView, channelContainer: UIView, channelThumbnailImage: UIImageView, channelTitleLabel: UILabel) {
+    /**
+    ||  thumbnailImage(120*90)(480*360), ratio is (1.3333333333)(0.75)
+    ||
+    ||   titleLabel
+    ||   infoLabel
+    ||
+    ||   *channelContainer*
+    ||   channelThumbnailImage  ||  channelTitleLabel
+    ||
+
+    :param: titleLabel
+    :param: infoLabel
+    :param: thumbnailImage
+    :param: channelContainer
+    :param: channelThumbnailImage
+    :param: channelTitleLabel
+    */
+    class func layoutVideoCollectionViewCell(thumbnailImage: UIImageView, titleLabel: UILabel, infoLabel: UILabel, channelContainer: UIView, channelThumbnailImage: UIImageView, channelTitleLabel: UILabel) {
 
     }
 
+    class func calculateVideoCollectionViewCellHeight(width: CGFloat) -> CGFloat {
+
+        return 0
+    }
 
 }
         
