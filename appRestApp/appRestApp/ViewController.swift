93a0fb42e79d6abc698873cf3d8894b6a52be600
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-04 21:52:49 +0800

    test autoCompleteSuggestionsWithSearchWish.

--- appRestApp/appRestApp/ViewController.swift
+++ appRestApp/appRestApp/ViewController.swift
@@ -9,25 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController, AuthorUserFetchingDelegate, UISearchBarDelegate {
-    
-    
+
+
     var popoverController: UIPopoverController?
     var popoverTableViewController: PopoverTableViewController?
-    
-    var rightBarItem : UIBarButtonItem?
-    
-    lazy var searchBar: AutoCompletePopoverSearchBar = {
-        let searchBar: AutoCompletePopoverSearchBar = AutoCompletePopoverSearchBar(frame: CGRectMake(0, 0, 300, 20))
+
+    var rightBarItem: UIBarButtonItem?
+
+    lazy var searchBar: UISearchBar = {
+        let searchBar: UISearchBar = UISearchBar(frame: CGRectMake(0, 0, 300, 20))
         searchBar.placeholder = "Searching..."
         searchBar.delegate = self
-        
+
         searchBar.backgroundColor = UIColor.clearColor()
         searchBar.showsCancelButton = true
         searchBar.userInteractionEnabled = true
-        
+
         return searchBar
-        }()
-    
+    }()
+
     @IBOutlet weak var loginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,82 +36,85 @@ class ViewController: UIViewController, AuthorUserFetchingDelegate, UISearchBarD
         if (YoutubeUserProfile.sharedInstance.hasLogin() == true) {
             loginButton.enabled = false
             loginButton.backgroundColor = UIColor.redColor()
-            
+
             //            self.startFetchingLoggedSubscriptionList() // used
         }
-        
+
         rightBarItem = UIBarButtonItem(customView: searchBar)
         //        searchBar.parentItem = rightBarItem
         self.navigationItem.rightBarButtonItem = rightBarItem
-        
-        popoverTableViewController = PopoverTableViewController()
-        popoverController = UIPopoverController(contentViewController: popoverTableViewController!)
     }
-    
+
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
+
+
     @IBAction func loginTapped(sender: AnyObject) {
         let viewController: GTMOAuth2ViewControllerTouch =
         GTMOAuth2ViewControllerTouch(scope: scope, clientID: kMyClientID, clientSecret: kMyClientSecret, keychainItemName: keychainItemName) {
             (controllerTouch, auth, error) -> Void in
-            
+
             if (error != nil) {
                 // Authentication failed
-                
+
             } else {
                 // Authentication succeeded
                 YoutubeUserProfile.sharedInstance.authorizeRequest(auth)
                 self.startFetchingLoggedSubscriptionList()
                 self.dismissViewControllerAnimated(true, completion: {
                     () -> Void in
-                    
+
                 })
             }
         }
-        
+
         let navCon: UINavigationController = UINavigationController(rootViewController: viewController)
         navCon.providesPresentationContextTransitionStyle = true
         navCon.definesPresentationContext = true
         navCon.modalPresentationStyle = .PageSheet
-        
-        
+
+
         self.presentViewController(navCon, animated: true) {
             () -> Void in
-            
+
         }
     }
-    
+
     func startFetchingLoggedSubscriptionList() {
         AuthoredFetcher.sharedInstance.initLoggedUser()
     }
-    
+
     // MARK: AuthorUserFetchingDelegate
     func endFetchingUserChannel(channel: GTLYouTubeChannel) {
     }
-    
+
     func endFetchingUserSubscriptions(array: NSArray) {
     }
-    
+
     // MARK: UISearchBarDelegate
     func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
         //        let theSearchBar: AutoCompleteSearchBar = searchBar as! AutoCompleteSearchBar
         //        theSearchBar.reloadData()
-        
+
         let searchWish = searchBar.text
         if (searchWish.isEmpty == false) {
             self.fetchAutoCompleteSuggestions(searchBar.text)
         }
+
+        showPopover()
+
     }
-    
+
     func searchBarCancelButtonClicked(searchBar: UISearchBar) {
         //        let theSearchBar: AutoCompleteSearchBar = searchBar as! AutoCompleteSearchBar
         //        theSearchBar.hideAutoCompleteView()
+
+        popoverController = nil
     }
-    
+
     func fetchAutoCompleteSuggestions(searchWish: String) {
         YoutubeDataFetcher.sharedInstance.autoCompleteSuggestionsWithSearchWish(searchWish, completeHandler: {
             (object, sucess) -> Void in
@@ -120,11 +123,20 @@ class ViewController: UIViewController, AuthorUserFetchingDelegate, UISearchBarD
             }
         })
     }
-    
-    func showPopover(){
+
+    func makePopoverController() {
+        popoverTableViewController = PopoverTableViewController()
+        popoverController = UIPopoverController(contentViewController: popoverTableViewController!)
+    }
+
+    func showPopover() {
+        if (popoverController != nil) {
+            return
+        }
+        makePopoverController()
         popoverController?.presentPopoverFromBarButtonItem(rightBarItem!, permittedArrowDirections: .Any, animated: true)
     }
-    
-    
+
+
 }
 
