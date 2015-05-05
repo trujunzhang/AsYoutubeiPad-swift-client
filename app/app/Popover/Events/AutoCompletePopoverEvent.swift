1c63b1c7e85fa5a02b22c7c0573499e5245effb3
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-05 17:43:27 +0800

    xxx

---
+++ app/app/Popover/Events/AutoCompletePopoverEvent.swift
@@ -0,0 +1,13 @@
+//
+//  AutoCompletePopoverEvent.swift
+//  app
+//
+//  Created by djzhang on 5/5/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import Foundation
+
+class AutoCompletePopoverEvent {
+    
+}
