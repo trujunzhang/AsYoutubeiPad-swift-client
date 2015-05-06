d0586434a75203a5b7e46b83fddef19de28a2184
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-06 13:47:19 +0800

    add AutoComplete.

--- app/app/CollectionView/UICollectionCells/VideoCollectionViewCellLayout.swift
+++ app/app/CollectionView/UICollectionCells/VideoCollectionViewCellLayout.swift
@@ -14,12 +14,11 @@ class VideoCollectionViewCellLayout {
 
     var thumbnailImage: UIImageView!
 
-    var infoContain: UIView!
+    var infoContainer: UIView!
     var totalTimeLabel: UILabel!
 
     var titleLabel: UILabel!
     var infoLabel: UILabel!
-    var channelContainer: UIView!
     var channelThumbnailImage: UIImageView!
     var channelTitleLabel: UILabel!
 
@@ -41,14 +40,65 @@ class VideoCollectionViewCellLayout {
     :param: channelTitleLabel
     */
     class func layoutVideoThumbnailImageContainer(thumbnailImage: UIImageView, totalTimeLabel: UILabel) {
+        layout(thumbnailImage, totalTimeLabel) {
+            view1, view2 in
 
+            view1.top == view1.superview!.top
+            view1.leading == view1.superview!.leading
+            view1.trailing == view1.superview!.trailing
+            view1.height == (view1.width / 4) * 3
+
+            view2.trailing == view2.superview!.trailing
+            view2.bottom == view2.superview!.bottom
+            view2.height == 10
+        }
     }
 
-    class func layoutVideoCollectionViewCell(thumbnailImage: UIImageView, titleLabel: UILabel, infoLabel: UILabel, channelContainer: UIView, channelThumbnailImage: UIImageView, channelTitleLabel: UILabel) {
+    class func layoutVideoTitleLabel(infoContainer: UIView) {
+        layout(infoContainer) {
+            view1 in
 
+            view1.leading == view1.superview!.leading
+            view1.trailing == view1.superview!.trailing
+            view1.bottom == view1.superview!.bottom
+            view1.height == COLLECTION_CELL_INFO_HEIGHT
+        }
+    }
+
+    class func layoutVideoTitleLabel(titleLabel: UILabel, infoLabel: UILabel) {
+        layout(titleLabel, infoLabel) {
+            view1 in
+
+            view1.leading == view1.superview!.leading
+            view1.trailing == view1.superview!.trailing
+            view1.top == view1.superview!.top + 4
+            view1.height == 36
+
+            view2.leading == view2.superview!.leading
+            view2.trailing == view2.superview!.trailing
+            view2.top == view1.superview!.bottom + 4
+            view2.height == 18
+        }
+    }
+
+    class func layoutVideoInfoContainer(channelThumbnailImage: UIImageView, channelTitleLabel: UILabel) {
+        layout(channelThumbnailImage, channelTitleLabel) {
+            view1 in
+
+            view1.leading == view1.superview!.leading
+            view1.bottom == view1.superview!.bottom - 4
+            view1.width == 24
+            view1.height == 24
+
+            view2.leading == view1.superview!.trailing + 4
+            view2.trailing == view2.superview!.trailing
+            view2.bottom == view2.superview!.bottom - 4
+            view2.height == 18
+        }
     }
 
     class func calculateVideoCollectionViewCellHeight(width: CGFloat) -> CGFloat {
+        let thumbnailHeight: CGFloat = (width / 4) * 3
 
         return 0
     }
--- app/app/CollectionView/YTVideoCollectionConstant.swift
+++ app/app/CollectionView/YTVideoCollectionConstant.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 let COLLECTION_CELL_INSET_VALUE: CGFloat = 5.0
+let COLLECTION_CELL_INFO_HEIGHT: CGFloat = 100
 
 extension YTVideosCollectionViewController {
 
