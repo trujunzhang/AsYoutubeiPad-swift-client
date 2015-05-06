da066a22051adbd38e5b7cb3195ce9fba9328f55
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-06 13:06:52 +0800

    add AutoComplete.

--- app/app/CollectionView/UICollectionCells/YTVideoCollectionViewCell.swift
+++ app/app/CollectionView/UICollectionCells/YTVideoCollectionViewCell.swift
@@ -13,84 +13,88 @@ import UIKit
 import WebImage
 
 class YTVideoCollectionViewCell: UICollectionViewCell {
-
-
+    
+    @IBOutlet weak var infoContain: UIView!
+    
+    @IBOutlet var thumbnailImage: UIImageView!
+    @IBOutlet weak var totalTimeLabel: UILabel!
+    
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var infoLabel: UILabel!
-    @IBOutlet var thumbnailImage: UIImageView!
+    
     @IBOutlet var channelContainer: UIView!
     @IBOutlet var channelThumbnailImage: UIImageView!
     @IBOutlet var channelTitleLabel: UILabel!
-
+    
     var imageUrl = ""
-
+    
     var videoCache: YoutubeVideoCache?
-
-
+    
+    
     func setupCell(_videoCache: YoutubeVideoCache) {
         videoCache = _videoCache
-
+        
         let videoTitle = YoutubeParser.getVideoSnippetTitle(videoCache)
         let thumbnailUrl = YoutubeModelParser.getVideoSnippetThumbnails(videoCache!)
         let channelTitle = YoutubeParser.getVideoSnippetChannelTitle(videoCache)
         let publishedAgo = YoutubeParser.getVideoSnippetChannelPublishedAt(videoCache)
-
+        
         // 1
         let url = NSURL(string: thumbnailUrl as String)
-//        thumbnailImage.hnk_setImageFromURL(url!) // used
-
-        let placeHolder = UIImage(named: "Travel")
-
-//        thumbnailImage.sd_setImageWithURL(url,placeHolder:placeHolder,options: SDWebImageDownloaderOptions.LowPriority)
+        //        thumbnailImage.hnk_setImageFromURL(url!) // used
         
-//        thumbnailImage.sd_setImageWithURL(url)
+        let placeHolder = UIImage(named: "Travel")
+        
+        //        thumbnailImage.sd_setImageWithURL(url,placeHolder:placeHolder,options: SDWebImageDownloaderOptions.LowPriority)
+        
+        //        thumbnailImage.sd_setImageWithURL(url)
         
         
         thumbnailImage.sd_setImageWithURL(url, placeholderImage: placeHolder, options: SDWebImageOptions.LowPriority )
-
-//        self.thumbnailImage.hnk_setImageFromURL(NSURL(string: self.imageUrl)!)// used
-
+        
+        //        self.thumbnailImage.hnk_setImageFromURL(NSURL(string: self.imageUrl)!)// used
+        
         // 2
         titleLabel.text = videoTitle
-
+        
         // 3
         infoLabel.text = publishedAgo
-
+        
         // 4
         channelTitleLabel.text = channelTitle
-
+        
         // 5
-//        setupChannelThumbnail()
+        //        setupChannelThumbnail()
     }
-
+    
     func setupChannelThumbnail() {
         let channelID = YoutubeParser.getChannelIdByVideo(videoCache)
-
+        
         if (self.imageUrl.isEmpty) {
             self.fetchChannelThumbnail(channelID)
         } else {
             self.channelThumbnailImage.sd_setImageWithURL(NSURL(string: self.imageUrl)!)
-//            self.channelThumbnailImage.hnk_setImageFromURL(NSURL(string: self.imageUrl)!)// used
+            //            self.channelThumbnailImage.hnk_setImageFromURL(NSURL(string: self.imageUrl)!)// used
         }
-
+        
     }
-
+    
     func fetchChannelThumbnail(channelID: NSString) {
         YoutubeFetcher.fetchChannelForThumbnail(channelID, completeHandler: {
             (object, sucess) -> Void in
             if (sucess == true) {
                 var array: NSArray = object as! NSArray
-
+                
                 var channel: MABYT3_Channel = array[0] as! MABYT3_Channel
                 var imageUrl = YoutubeModelParser.getMABChannelThumbnalUrl(channel)
-
+                
                 self.imageUrl = imageUrl as String
-
+                
                 self.channelThumbnailImage.sd_setImageWithURL(NSURL(string: self.imageUrl)!)
-//                self.channelThumbnailImage.hnk_setImageFromURL(NSURL(string: self.imageUrl)!)// used
+                //                self.channelThumbnailImage.hnk_setImageFromURL(NSURL(string: self.imageUrl)!)// used
             }
         })
     }
-
-
+    
+    
 }
