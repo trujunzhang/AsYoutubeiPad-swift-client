a5885d14c1338b2783a05ddd8f78f4d6fded3009
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-10 19:38:42 +0800

    xxx

--- app/app/DetailPage/DetailPageTableViewController.swift
+++ app/app/DetailPage/DetailPageTableViewController.swift
@@ -8,23 +8,29 @@
 
 import UIKit
 
-class DetailPageTableViewController: UITableViewController {
-
+class DetailPageTableViewController: UITableViewController,UITableViewDelegate,UITableViewDataSource {
+    
     var videoInfoObject: VideoInfoObject = VideoInfoObject()
-
+    
     var pageSections:[DetailPageSection] = [DetailPageSection]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
         self.makeVideoInfoSection() // test
     }
-
+    
     func makeVideoInfoSection(){
         let section:DetailPageSection = DetailPageSection.makeVideoInfoSection(self.videoInfoObject)
         self.pageSections.append(section)
+        
+        self.tableView.reloadData()
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -33,76 +39,62 @@ class DetailPageTableViewController: UITableViewController {
     func addVideoInfo(videoInfoObject: VideoInfoObject){
         self.videoInfoObject = videoInfoObject
     }
-
-    // MARK: - Table view data source
-
+    
+    
+    
+    //MARK: UITableViewDataSource
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 0
+        return self.pageSections.count
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return 0
+        let section:DetailPageSection = self.pageSections[section]
+        return section.rowObjects.count
     }
-
-
-
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
-
-        // Configure the cell...
-
+        let section:DetailPageSection = self.pageSections[indexPath.section]
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(section.identifer, forIndexPath: indexPath) as! UITableViewCell
+        configureCell(cell, forRowAtIndexPath: indexPath)
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
+    
+    func configureCell(cell: UITableViewCell, forRowAtIndexPath: NSIndexPath) {
+         let section:DetailPageSection = self.pageSections[forRowAtIndexPath.section]
+        
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    //MARK: UITableViewDelegate
+    
+    override  func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
+    
+    override  func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 0
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
+    
+    override  func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    
+    override  func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0
     }
-    */
-
+    
+    override  func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    
+    override  func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        
+        return UIView()
+    }
+    
+    
+    
+    
 }
