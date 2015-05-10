8311e4b274c9340672b646ab552e489c19a4c46b
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-10 19:46:35 +0800

    xxx

--- app/app/DetailPage/DetailPageTableUtils.swift
+++ app/app/DetailPage/DetailPageTableUtils.swift
@@ -12,20 +12,28 @@ let VIDEO_INFO_CELL_IDENTIFER = "videoInfoIdentifier"
 let CHANNEL_INFO_CELL_IDENTIFER = "channelInfoIdentifier"
 let SUGGESTION_CELL_IDENTIFER = "suggestionVideoInfoIdentifier"
 
+public enum DetailPageCellIdentifier {
+    case VideoInfoCellIdentifier
+    case ChannelInfoCellIdentifier
+    case SuggestionListCellIdentifier
+}
+
 
 
 class DetailPageSection{
+    var sectionIdentifier :    DetailPageCellIdentifier?
     var sectionTitle = ""
     var identifer = ""
     var rowObjects :[AnyObject] = [AnyObject]()
-
-   class func makeVideoInfoSection(object:AnyObject) -> DetailPageSection{
+    
+    class func makeVideoInfoSection(object:AnyObject) -> DetailPageSection{
         let section: DetailPageSection = DetailPageSection()
-
+        
         section.sectionTitle = ""
+        section.sectionIdentifier = DetailPageCellIdentifier.VideoInfoCellIdentifier
         section.identifer = VIDEO_INFO_CELL_IDENTIFER
         section.rowObjects.append(object)
-
+        
         return section
     }
 }
