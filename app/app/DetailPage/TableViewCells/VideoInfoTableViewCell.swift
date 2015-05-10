7bdc29440d6716e120ed7f65e265317e80d705e2
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-10 10:59:20 +0800

    xxx

---
+++ app/app/DetailPage/TableViewCells/VideoInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideoInfoTableViewCell.swift
+//  app
+//
+//  Created by djzhang on 5/10/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import UIKit
+
+class VideoInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
