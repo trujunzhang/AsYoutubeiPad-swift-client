fdaa221e6354cca471c30a975ca0e53a8ff72619
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-13 14:42:54 +0800

    xxx

--- app/app/DetailPage/DetailPageTableUtils.swift
+++ app/app/DetailPage/DetailPageTableUtils.swift
@@ -111,6 +111,20 @@ class DetailPageSection : NSObject{
     
     
     // MARK: suggestion section
+    class func makeSectionTitleSection(array:NSArray) -> DetailPageSection{
+        let section: DetailPageSection = DetailPageSection()
+        
+        section.sectionTitle = "Suggestions"
+        section.rowHeight = 80
+        section.sectionHeaderHeight = 0
+        section.sectionFooterHeight = 20
+        section.sectionIdentifier = DetailPageCellIdentifier.SuggestionListCellIdentifier
+        section.identifer = SUGGESTION_CELL_IDENTIFER
+        section.rowObjects = array as [AnyObject]
+        
+        return section
+    }
+
     class func makeSuggestionVideoListSection(array:NSArray) -> DetailPageSection{
         let section: DetailPageSection = DetailPageSection()
         
